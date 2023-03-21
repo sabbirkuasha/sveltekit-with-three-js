7d729b5bc72bac627e9dfce06899821e221e84fc
sabbirz <sabbir.kuasha@gmail.com>
2023-03-21 12:37:11 +0600

    Initial commit

---
+++ src/lib/SceneOne.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { PerspectiveCamera, DirectionalLight, AmbientLight, Mesh } from '@threlte/core';
+	import { MeshStandardMaterial, IcosahedronGeometry } from 'three';
+	import { tweened } from 'svelte/motion';
+
+	const t = tweened(3, { duration: 2000 });
+	setTimeout(() => {
+		t.set(-3);
+	});
+</script>
+
+<PerspectiveCamera position={{ y: 0, z: 20 }} lookAt={{ x: 0, y: 0, z: 0 }} />
+
+<DirectionalLight />
+<AmbientLight />
+
+<Mesh
+	geometry={new IcosahedronGeometry()}
+	material={new MeshStandardMaterial({ color: 'seagreen' })}
+	position={{ x: 0, y: 0, z: 15 }}
+	rotation={{ x: 0, y: $t, z: 0 }}
+/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import SceneOne from '../lib/SceneOne.svelte';
+</script>
+
+<section class="p-5">
+	<h2 class="mb-5">Scene One</h2>
+
+	<div class="canvas-wrapper ring h-[500px]">
+		<Canvas>
+			<SceneOne />
+			<!-- SceneOne -->
+		</Canvas>
+	</div>
+</section>
